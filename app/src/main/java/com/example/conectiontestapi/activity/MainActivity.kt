af11c82b48dff2104231918ecbdc0f1f4441484d
LEANDRO <leandroleo4916@gmail.com>
2021-04-04 12:39:11 -0300

    First Commit

--- app/src/main/java/com/example/conectiontestapi/activity/MainActivity.kt
+++ app/src/main/java/com/example/conectiontestapi/activity/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.conectiontestapi.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.conectiontestapi.R
+import com.example.conectiontestapi.api.PersonApi
+import com.example.conectiontestapi.api.PotterApi
+import com.example.conectiontestapi.api.Retrofit
+import com.example.conectiontestapi.model.AdapterPotter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,11 +25,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(findViewById<View>(R.id.toolbar) as Toolbar)
-        //hide icon back from tollbar
+        //esconde icone voltar na toolbar
         supportActionBar!!.setDisplayHomeAsUpEnabled(false)
 
         recyclerView = findViewById(R.id.recycler_potter)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        getData()
+
+    }
+
+    private fun getData(){
+
+        val retrofit = Retrofit.createService(PotterApi::class.java)
+
+        val call: Call<List<PersonApi>> = retrofit.getPotterApi()
+
+        call.enqueue(object: Callback<List<PersonApi>>{
+            override fun onResponse(call: Call<List<PersonApi>>, response: Response<List<PersonApi>>){
+                val adapter = AdapterPotter(this@MainActivity, response.body()!!.toList())
+                recyclerView.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<PersonApi>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/conectiontestapi/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/conectiontestapi/activity/ProfileActivity.kt
@@ -7,6 +7,14 @@ import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.conectiontestapi.R
+import com.example.conectiontestapi.api.PersonApi
+import com.example.conectiontestapi.api.PotterApi
+import com.example.conectiontestapi.api.Retrofit
+import com.example.conectiontestapi.model.AdapterPotter
+import com.example.conectiontestapi.model.AdapterProfile
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -17,12 +25,31 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         setSupportActionBar(findViewById<View>(R.id.toolbar_profile) as Toolbar)
-        //show icon back from tollbar
+        //Mostra o icone voltar na tollbar
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         recyclerView = findViewById(R.id.recycler_profile)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        getData()
+    }
+
+    private fun getData(){
+
+        val remote = Retrofit.createService(PotterApi::class.java)
+        val call: Call<List<PersonApi>> = remote.getPotterApi()
+        call.enqueue(object: Callback<List<PersonApi>>{
+            override fun onResponse(call: Call<List<PersonApi>>, response: Response<List<PersonApi>>) {
+                val adapter = AdapterProfile(this@ProfileActivity, response.body()!!.toList())
+
+            }
+
+            override fun onFailure(call: Call<List<PersonApi>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
 
     }
+
 }
